e9b69b2cfb1f0c2ca567cb6c9aa5fb2b72edcb4e
Alvaro Figueiredo Neto <af4722593@gmail.com>
2025-08-06 03:05:18 -0300

    terminei a primeira aula
    printei a quantidade de vez que aparece cada valor, mudei as siglas de senioridade para palavras cheias, e descrevi em uma tabela ja com valores atualizados

--- projeto.py
+++ projeto.py
@@ -16,3 +16,51 @@ linhas, colunas = df.shape[0], df.shape[1]
 
 print('O número de linhas é:', linhas)
 print('colunas:', colunas)
+
+print("\n")
+
+substituir_nivel_de_experiencia = {
+    'SE': 'Senior',
+    'MI': 'Pleno',
+    'EN': 'Junior',
+    'EX': 'Executivo'
+}
+
+df["nivel_de_experiencia"] = df["nivel_de_experiencia"].replace(substituir_nivel_de_experiencia)
+
+
+print(df["nivel_de_experiencia"].value_counts()) # - me passa a quantidade de vezes que cada senioridade aparece nos dados.
+
+print("\n")
+
+print(df["tipo_de_emprego"].value_counts()) # - me passa o tipo de contrato de trabalho dos funcionarios.
+
+print("\n")
+
+print(df["ano"].value_counts())
+
+print("\n")
+
+print(df["cargo"].value_counts())
+
+print("\n")
+
+print(df["salario_em_dolar"].value_counts())
+
+print("\n")
+
+print(df["localizacao_da_empresa"].value_counts())
+
+print("\n")
+
+print(df["local_da_residencia_do_funcionario"].value_counts())
+
+print("\n")
+
+print(df["proporcao_remota"].value_counts())
+
+print("\n")
+
+print(df["tamanho_da_empresa"].value_counts())
+
+print(df.describe(include="object"))
